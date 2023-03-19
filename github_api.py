9a76fcc54f42136c12ce018b8e757641087fda55
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-03-19 15:57:32 -0400

    github api

---
+++ github_api.py
@@ -0,0 +1,34 @@
+import requests
+
+def create_query(languages, min_stars=50000):
+    query = f"stars:>{min_stars} "
+
+    for language in languages:
+        query += f"language:{language} "
+
+    return query
+
+
+def repos_with_most_stars(languages, sort="stars", order="desc"):
+    gh_api_url = "https://api.github.com/search/repositories"
+    
+    query = create_query(languages)
+    params = {"q": query, "sort": sort, "order": order}
+
+    response = requests.get(gh_api_url, params)
+
+    response_json = response.json()["items"]
+    return response_json
+
+if __name__ == "__main__":
+    languages = ["Python", "Javascript", "Go"]
+    results = repos_with_most_stars(languages)
+    # print(len(results))
+
+    for result in results:
+        language = result["language"]
+        stars = result["stargazers_count"]
+        name = result["name"]
+
+        print(f"-> {name} is a {language} repo with {stars} stars")
+
